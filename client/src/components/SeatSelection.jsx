b9b30733c4ff6a4dde6a7260166c1e6730698b4a
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-10-31 17:30:55 +0100

    edit seat selection component

--- client/src/components/SeatSelection.jsx
+++ client/src/components/SeatSelection.jsx
@@ -13,23 +13,25 @@ function SeatSelection({ selectedSeats, setSelectedSeats, bus }) {
 
   return (
     <div>
-      <div className="bus-container w-[400px]">
+      <div className="border-none w-full ">
         <Card>
-          <Row gutter={[0, 8]}>
+          <Row
+          className="rounded-md border p-3 border-pink-500"
+          gutter={[0, 8]}>
             {Array.from(Array(capacity).keys()).map((seat) => {
               let seatClass = ``;
               selectedSeats.includes(seat + 1);
               if (selectedSeats.includes(seat + 1)) {
-                seatClass = `bg-orange-500 border-[2px] border-orange-500 text-center text-black text-base font-mono p-1 cursor-pointer w-10 h-10 rounded-tl-3xl rounded-br-3xl `;
+                seatClass = `bg-pink-500 border-[1px] font-bold rounded-md border-pink-500 text-center text-black text-base p-1 cursor-pointer w-10 h-10`;
               } else if (bus.seatsBooked.includes(seat + 1)) {
-                seatClass = ` bg-gray-500 border-[2px] border-gray-500 text-center text-black text-base font-mono p-1 pointer-events-none cursor-not-allowed w-10 h-10 rounded-tl-3xl rounded-br-3xl `;
+                seatClass = ` bg-gray-500 border-[1px] border-gray-500 rounded-md text-center font-bold text-black text-base p-1 pointer-events-none cursor-not-allowed w-10 h-10`;
 
                 // seatClass = `bg-black border-[2px] border-black text-center text-white text-base font-mono p-1 cursor-pointer w-10 h-10 rounded-tl-3xl rounded-br-3xl `;
               }
               return (
-                <Col span={6}>
-                  <div className="flex justify-center">
-                    <div className="border-[2px] border-gray-500 text-center text-black text-base font-mono p-1  w-10 h-10 rounded-tl-3xl rounded-br-3xl ">
+                <Col span={3}>
+                  <div className="flex gap-2 justify-center">
+                    <div className="border-[1px] border-gray-500 cursor-pointer rounded-md text-center font-bold text-black text-base  p-1  w-10 h-10  ">
                       <div
                         className={`seat ${seatClass}`}
                         onClick={() => selectOrUnselectSeats(seat + 1)}
