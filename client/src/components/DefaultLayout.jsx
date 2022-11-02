969b749b997b311cca61497c09775fe4414c93b6
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-11-02 09:19:52 +0100

    edit header

--- client/src/components/DefaultLayout.jsx
+++ client/src/components/DefaultLayout.jsx
@@ -107,23 +107,16 @@ function DefaultLayout({ children }) {
         </div>
       </div>
 
-      <div className="body w-full">
-        <div className="header shadow-lg w-full p-5  ">
-          {collapsed ? (
-            <MenuIcon
-              className="cursor-pointer text-[30px]"
-              onClick={() => {
-                setCollapsed(false);
-              }}
-            />
-          ) : (
-            <CloseIcon
-              className="cursor-pointer text-[30px]"
-              onClick={() => {
-                setCollapsed(true);
-              }}
-            />
-          )}
+      <div className="body w-screen relative right-4">
+        <div className="header shadow-lg w-full p-5   ">
+      <div className="pt-2 relative mx-auto text-gray-600">
+        <input className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" type="search" name="search" placeholder="Search" />
+        <button type="submit" className="absolute  top-0 mt-5 left-52">
+          <svg className="text-gray-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 56.966 56.966" style={{enableBackground: 'new 0 0 56.966 56.966'}} xmlSpace="preserve" width="512px" height="512px">
+            <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+          </svg>
+        </button>
+      </div>
         </div>
         <div className="content p-[10px]">{children}</div>
       </div>
